e36d4005e7e1f47af1bda5ecf28f81c4fc8651c3
Kevin Phan <kevin2006phan@gmail.com>
2025-08-23 01:07:57 +1000

    updated lsp

--- nvim/lua/plugins/lsp/lspconfig.lua
+++ nvim/lua/plugins/lsp/lspconfig.lua
@@ -3,11 +3,12 @@ return {
     config = function()
         require("nvchad.configs.lspconfig").defaults()
 
-        local servers = { "html", "cssls", "clangd" }
+        local servers = { "html", "cssls", "clangd", "omnisharp" }
         vim.lsp.enable(servers)
 
         -- read :h vim.lsp.config for changing options of lsp servers
         require("lspconfig").lua_ls.setup {}
         require("lspconfig").clangd.setup {}
+        require("lspconfig").omnisharp.setup {}
     end,
 }
